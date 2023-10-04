11e316ac2f1a98054571549e324cf92c53b885cc
sturmenta <sturmenta@gmail.com>
2023-10-04 12:35:00 -0300

    follow supabase guide step by step

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # readme
 
-1) supabase quick-start https://docs.plasmo.com/quickstarts/with-supabase
-2) generate new keys chrome web store https://developer.chrome.com/docs/extensions/mv3/manifest/key/
+1) [plasmo-supabase quick-start](https://docs.plasmo.com/quickstarts/with-supabase)
+2) [generate new extension keys with chrome web store](https://developer.chrome.com/docs/extensions/mv3/manifest/key/)
+3) [supabase google login](https://supabase.com/docs/guides/auth/social-login/auth-google#using-native-sign-in-for-chrome-extensions)
--- options.tsx
+++ options.tsx
@@ -81,6 +81,51 @@ function IndexOptions() {
     })
   }
 
+  const handleLoginWithGoogle = async () => {
+    const manifest = chrome.runtime.getManifest()
+
+    const url = new URL("https://accounts.google.com/o/oauth2/auth")
+
+    url.searchParams.set("client_id", manifest.oauth2.client_id)
+    url.searchParams.set("response_type", "id_token")
+    url.searchParams.set("access_type", "offline")
+    url.searchParams.set(
+      "redirect_uri",
+      `https://${chrome.runtime.id}.chromiumapp.org`
+    )
+    url.searchParams.set("scope", manifest.oauth2.scopes.join(" "))
+
+    console.log(`url`, url)
+
+    chrome.identity.launchWebAuthFlow(
+      {
+        url: url.href,
+        interactive: true
+      },
+      async (redirectedTo) => {
+        if (chrome.runtime.lastError) {
+          // auth was not successful
+          console.log(
+            "There was an error with the authentication: ",
+            chrome.runtime.lastError.message
+          )
+        } else {
+          // auth was successful, extract the ID token from the redirectedTo URL
+          const url = new URL(redirectedTo)
+          const params = new URLSearchParams(url.hash)
+
+          const { data, error } = await supabase.auth.signInWithIdToken({
+            provider: "google",
+            token: params.get("id_token")
+          })
+
+          console.log(`error`, error)
+          console.log(`data`, data)
+        }
+      }
+    )
+  }
+
   return (
     <main
       style={{
@@ -148,6 +193,13 @@ function IndexOptions() {
                 handleOAuthLogin("github")
               }}>
               Sign in with GitHub
+            </button>
+
+            <button
+              onClick={(e) => {
+                handleLoginWithGoogle()
+              }}>
+              Sign in with Google
             </button>
           </>
         )}
