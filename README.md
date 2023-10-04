8e320e8d1583f348e54354a8d13b329edba683b1
sturmenta <sturmenta@gmail.com>
2023-10-04 17:15:07 -0300

    get token oauth2 🔥

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 2) [generate new extension keys with chrome web store](https://developer.chrome.com/docs/extensions/mv3/manifest/key/)
 3) [supabase google login](https://supabase.com/docs/guides/auth/social-login/auth-google#using-native-sign-in-for-chrome-extensions)
     - code and guide not totally working
-        - images for working config:
+        - images for working config (supabase auth v1):
 
 <!-- ![](https://github.com/sturm-dev/plasmo-supabase-boilerplate/assets/30802967/f8c7ea2e-db3b-425e-9a61-7105cdfb4c18) -->
 ![](https://github-production-user-asset-6210df.s3.amazonaws.com/30802967/272638141-f8c7ea2e-db3b-425e-9a61-7105cdfb4c18.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIWNJYAX4CSVEH53A%2F20231004%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20231004T160549Z&X-Amz-Expires=300&X-Amz-Signature=26493b5ab7faae3d1f5b5e99b41fcaeedfca8882c813a29bc2f4801cdda49924&X-Amz-SignedHeaders=host&actor_id=30802967&key_id=0&repo_id=700401250)
@@ -17,3 +17,17 @@
 
 <!-- ![](https://github.com/sturm-dev/plasmo-supabase-boilerplate/assets/30802967/1b83f7ed-8dd0-48e7-ae6f-cf15a1d7867a) -->
 ![](https://github-production-user-asset-6210df.s3.amazonaws.com/30802967/272638173-1b83f7ed-8dd0-48e7-ae6f-cf15a1d7867a.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIWNJYAX4CSVEH53A%2F20231004%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20231004T160730Z&X-Amz-Expires=300&X-Amz-Signature=9335c26ce00c5320893eaf9a0443b408dd97001bb25e550eeacae8e247254756&X-Amz-SignedHeaders=host&actor_id=30802967&key_id=0&repo_id=700401250)
+
+3.1) valid config for the (OAuth 2) [supabase google login guide](https://supabase.com/docs/guides/auth/social-login/auth-google#using-native-sign-in-for-chrome-extensions)
+
+<!-- ![](https://github.com/sturm-dev/plasmo-supabase-boilerplate/assets/30802967/c1bdad90-5eb4-4d46-8d42-3cfec95bd05e) -->
+![](https://github-production-user-asset-6210df.s3.amazonaws.com/30802967/272697969-c1bdad90-5eb4-4d46-8d42-3cfec95bd05e.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIWNJYAX4CSVEH53A%2F20231004%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20231004T201046Z&X-Amz-Expires=300&X-Amz-Signature=185f824b64904d2836b116def7b7d1c3869326aa58f36beb1e250d2dbdfb406a&X-Amz-SignedHeaders=host&actor_id=30802967&key_id=0&repo_id=700401250)
+
+<!-- ![](https://github.com/sturm-dev/plasmo-supabase-boilerplate/assets/30802967/4cdf84b4-d44c-49f4-aeee-2a6707983d69) -->
+![](https://github-production-user-asset-6210df.s3.amazonaws.com/30802967/272697979-4cdf84b4-d44c-49f4-aeee-2a6707983d69.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIWNJYAX4CSVEH53A%2F20231004%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20231004T201100Z&X-Amz-Expires=300&X-Amz-Signature=8976df2bf90ef7399c12b3fbff166dfd3b7284fa500f5f1fc1fb0a52aa5956e4&X-Amz-SignedHeaders=host&actor_id=30802967&key_id=0&repo_id=700401250)
+
+<!-- ![](https://github.com/sturm-dev/plasmo-supabase-boilerplate/assets/30802967/5c101d30-a8d0-46f7-84e8-eea7e6fdceb4) -->
+![](https://github-production-user-asset-6210df.s3.amazonaws.com/30802967/272697984-5c101d30-a8d0-46f7-84e8-eea7e6fdceb4.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIWNJYAX4CSVEH53A%2F20231004%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20231004T201111Z&X-Amz-Expires=300&X-Amz-Signature=8dc8e30174eea72405635428426a5804a2446a848e07f430af99d2a5b8ab8871&X-Amz-SignedHeaders=host&actor_id=30802967&key_id=0&repo_id=700401250)
+
+<!-- ![](https://github.com/sturm-dev/plasmo-supabase-boilerplate/assets/30802967/e1cd9594-fa71-44bf-8a17-207b499d06ff) -->
+![](https://github-production-user-asset-6210df.s3.amazonaws.com/30802967/272697987-e1cd9594-fa71-44bf-8a17-207b499d06ff.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIWNJYAX4CSVEH53A%2F20231004%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20231004T201125Z&X-Amz-Expires=300&X-Amz-Signature=694a74684f76b6fbf138f86323f2abe45f1012fc45468e98fa605d0130d24e88&X-Amz-SignedHeaders=host&actor_id=30802967&key_id=0&repo_id=700401250)
--- options.tsx
+++ options.tsx
@@ -83,51 +83,22 @@ function IndexOptions() {
     })
   }
 
-  // code from guide (not working) -> https://supabase.com/docs/guides/auth/social-login/auth-google#using-native-sign-in-for-chrome-extensions
-  const handleLoginWithGoogle_1 = async () => {
-    const manifest = chrome.runtime.getManifest()
+  const testYoutubeApiWithToken = async ({
+    auth_token
+  }: {
+    auth_token: string
+  }) => {
+    // video id from the same user than the one logged in
+    const { data: getYoutubeVideoInfo_data, error: getYoutubeVideoInfo_error } =
+      await getYoutubeVideoInfo({ auth_token, video_id: "LSqzQsBL6r8" })
+    if (getYoutubeVideoInfo_error) throw { getYoutubeVideoInfo_error }
+    // ERROR -> "Request had invalid authentication credentials. Expected OAuth 2 access token, login cookie or other valid authentication credential. See https://developers.google.com/identity/sign-in/web/devconsole-project."
+    // It's seems that the token from auth/v1 is not valid for the youtube api (need for a OAuth 2 access token)
 
-    const url = new URL("https://accounts.google.com/o/oauth2/auth")
-
-    url.searchParams.set("client_id", manifest.oauth2.client_id)
-    url.searchParams.set("response_type", "id_token")
-    url.searchParams.set("access_type", "offline")
-    url.searchParams.set(
-      "redirect_uri",
-      `https://${chrome.runtime.id}.chromiumapp.org`
-    )
-    url.searchParams.set("scope", manifest.oauth2.scopes.join(" "))
-
-    chrome.identity.launchWebAuthFlow(
-      {
-        url: url.href,
-        interactive: true
-      },
-      async (redirectedTo) => {
-        if (chrome.runtime.lastError) {
-          // auth was not successful
-          console.log(
-            "There was an error with the authentication: ",
-            chrome.runtime.lastError.message
-          )
-        } else {
-          // auth was successful, extract the ID token from the redirectedTo URL
-          const url = new URL(redirectedTo)
-          const params = new URLSearchParams(url.hash)
-
-          const { data, error } = await supabase.auth.signInWithIdToken({
-            provider: "google",
-            token: params.get("id_token")
-          })
-
-          console.log(`error`, error)
-          console.log(`data`, data)
-        }
-      }
-    )
+    console.log(`getYoutubeVideoInfo_data`, getYoutubeVideoInfo_data)
   }
 
-  const handleLoginWithGoogle_2 = async () => {
+  const handleLoginWithGoogle_auth_v1 = async () => {
     try {
       // NOTE: add this to your supabase auth redirect URLs list (Authentication > URL Configuration > Redirect URLs)
       const redirectUri = chrome.identity.getRedirectURL("supabase-auth")
@@ -155,22 +126,66 @@ function IndexOptions() {
 
       const session = setSession_result.data.session
       console.log(`session`, session)
-
-      const {
-        data: getYoutubeVideoInfo_data,
-        error: getYoutubeVideoInfo_error
-      } = await getYoutubeVideoInfo({
-        auth_token: session.access_token,
-        video_id: "LSqzQsBL6r8" // video id from the same user than the one logged in
-      })
-      if (getYoutubeVideoInfo_error) throw { getYoutubeVideoInfo_error }
-      // ERROR -> "Request had invalid authentication credentials. Expected OAuth 2 access token, login cookie or other valid authentication credential. See https://developers.google.com/identity/sign-in/web/devconsole-project."
-      // It's seems that the token from auth/v1 is not valid for the youtube api (need for a OAuth 2 access token)
-
-      console.log(`getYoutubeVideoInfo_data`, getYoutubeVideoInfo_data)
     } catch (error) {
       console.error(`error`, error)
     }
+  }
+
+  const handleLoginWithGoogle_oauth_2 = async () => {
+    const manifest = chrome.runtime.getManifest()
+
+    const url = new URL("https://accounts.google.com/o/oauth2/auth")
+    const redirectUri = chrome.identity.getRedirectURL("supabase-auth")
+    console.log(`redirectUri`, redirectUri)
+    // -> https://dekkcnhhmkcahchnaphlibnodmigphnc.chromiumapp.org/supabase-auth
+    // save on google cloud console > credentials > OAuth 2.0 Client IDs > Web client
+    // save on supabase > Authentication > URL Configuration > Redirect URLs
+    // scopes on manifest - only ["openid", "email", "profile"]
+
+    url.searchParams.set("client_id", manifest.oauth2.client_id)
+    url.searchParams.set("response_type", "id_token")
+    url.searchParams.set("access_type", "offline")
+    url.searchParams.set("redirect_uri", redirectUri)
+    url.searchParams.set("scope", manifest.oauth2.scopes.join(" "))
+
+    console.log(`url`, url)
+
+    console.log(`url.href`, url.href)
+
+    chrome.identity.launchWebAuthFlow(
+      {
+        url: url.href,
+        interactive: true
+      },
+      async (redirectedTo) => {
+        if (chrome.runtime.lastError) {
+          // auth was not successful
+          console.log(
+            "There was an error with the authentication: ",
+            chrome.runtime.lastError.message
+          )
+        } else {
+          console.log("auth was successful")
+          console.log(`redirectedTo`, redirectedTo)
+
+          // auth was successful, extract the ID token from the redirectedTo URL
+          const url = new URL(redirectedTo)
+          const params = new URLSearchParams(url.hash.replace("#", ""))
+          const token = params.get("id_token")
+
+          console.log(`auth was successful - url`, url)
+          console.log(`auth was successful - token`, token)
+
+          const { data, error } = await supabase.auth.signInWithIdToken({
+            provider: "google",
+            token
+          })
+
+          console.log(`error`, error)
+          console.log(`data`, data)
+        }
+      }
+    )
   }
 
   return (
@@ -244,7 +259,7 @@ function IndexOptions() {
 
             <button
               onClick={(e) => {
-                handleLoginWithGoogle_2()
+                handleLoginWithGoogle_oauth_2()
               }}>
               Sign in with Google
             </button>
